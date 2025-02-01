8ebdb4898e1987db94b41c3665765df3091bb51b
erikth97 <erik.tamayohe@uanl.edu.mx>
2025-01-31 18:47:36 -0600

    Config: Add React Router DOM to Project

---
+++ src/router.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginView from "./views/loginView";
+import RegisterView from "./views/registerView";
+
+export default function Router() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route >
+                    <Route path="/auth/login" element={<LoginView />} />
+                    <Route path="/auth/register" element={<RegisterView />} />
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
+);
+}   
