24a2044d635a42b913d15005233d8c7dc01bcaa7
erikth97 <erik.tamayohe@uanl.edu.mx>
2025-03-26 18:00:47 -0600

    Feat: Add Enviromentvariables

--- src/views/registerView.tsx
+++ src/views/registerView.tsx
@@ -14,14 +14,16 @@ export default function RegisterView() {
     password_confirmation: ''
   }
 
-  const { register, watch, handleSubmit, formState: { errors } } = useForm({defaultValues : initialValues})
+  const { register, watch, reset,handleSubmit, formState: { errors } } = useForm({defaultValues : initialValues})
 
   const password = watch('password')
 
   const handleRegister = async (formData : RegisterForm) => {
     try {
-      const {data} = await axios.post('http://localhost:400/auth/register', formData)
+      const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, formData)
       console.log(data)
+
+      reset()
     } catch (error) {
       if(isAxiosError(error) && error.response) {
         console.log(error.response.data.error)
